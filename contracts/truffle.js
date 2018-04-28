c8c1e8327e67f2dd09352d4880934eb1aceb4b08
Vladimir Khramov <quant13@gmail.com>
2018-04-28 16:24:07 +0300

    truffle init

---
+++ contracts/truffle.js
@@ -0,0 +1,23 @@
+module.exports = {
+    networks: {
+        development: {
+            host: "localhost",
+            port: 9545,
+            gasPrice: 1,
+            network_id: "*" // Match any network id
+        },
+
+        rinkeby: {  // testnet
+            host: "localhost",
+            port: 8547,
+            network_id: 4
+        }
+    },
+    solc: {
+        optimizer: {
+            enabled: true,
+            runs: 200
+        }
+    }
+
+};
